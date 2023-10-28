8531531cb322a5b9317198dfb725c4c5341c8f5d
Enes Deniz <denizdeniz190303@gmail.com>
2023-10-28 22:31:47 +0300

    v1

---
+++ src/main/java/class01.java
@@ -0,0 +1,18 @@
+public class class01 {
+    public static void main(String[] args) {
+        //version 1
+        System.out.println("Versiyon 1 kodları");
+
+        //version 2
+        System.out.println("versiyon 2 kodları");
+
+
+
+
+
+
+
+
+
+    }
+}
